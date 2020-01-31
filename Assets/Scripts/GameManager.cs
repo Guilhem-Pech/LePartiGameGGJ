e7985a38c69df3ee990e6fbde70b36160e64a361
Guilhem PECH <guilhem.pech@gmail.com>
2020-02-01 00:32:04 +0100

    :construction: Make the GameManager a singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(GameManager))]
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+       if(Instance != null && Instance != this)
+           Destroy(gameObject);
+       else  Instance = this;
+    }
+
+   
 }
