4fb5c8f3ff1101ebf2df2912fbb08cf9e904d493
Guilhem PECH <guilhem.pech@gmail.com>
2020-02-01 00:33:44 +0100

    :fire: Remove nonsense

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(GameManager))]
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
@@ -13,6 +11,5 @@ public class GameManager : MonoBehaviour
            Destroy(gameObject);
        else  Instance = this;
     }
-
-   
+  
 }
