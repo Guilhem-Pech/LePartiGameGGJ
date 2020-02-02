8ff6969ab20a2082bda5f1a583ab7c473a9d545a
Guilhem PECH <guilhem.pech@gmail.com>
2020-02-02 16:11:45 +0100

    :fire: souuuund

---
+++ Assets/States/Environement/Background/OnStartStorm.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace States.Environement.Background
+{
+    public class OnStartStorm : StateMachineBehaviour
+    {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex)
+        {
+            AkSoundEngine.PostEvent("Stop_Ambiance_calme", animator.gameObject);
+            AkSoundEngine.PostEvent("Play_Storm", animator.gameObject);
+            AkSoundEngine.PostEvent("Woosh_Nuage", animator.gameObject);
+        }
+    }
+}
---
+++ Assets/States/Environement/Background/OnStopStorm.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace States.Environement.Background
+{
+    public class OnStopStorm : StateMachineBehaviour
+    {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex)
+        {
+            AkSoundEngine.PostEvent("Stop_Storm", animator.gameObject);
+        }
+    }
+}
