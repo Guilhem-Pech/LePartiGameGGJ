7b739cb15b9ee2f12e021ef36eccb686a6bcd6d3
Guilhem PECH <guilhem.pech@gmail.com>
2020-02-08 16:30:35 +0100

    FixStopSound

--- Assets/States/Environement/EndTitle/OnCredit.cs
+++ Assets/States/Environement/EndTitle/OnCredit.cs
@@ -6,7 +6,7 @@ namespace States.Environement.EndTitle
     {
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            AkSoundEngine.PostEvent("Stop_Ambiance_Calme", null);
+            AkSoundEngine.PostEvent("Stop_Ambiance_Calme", animator.gameObject);
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
