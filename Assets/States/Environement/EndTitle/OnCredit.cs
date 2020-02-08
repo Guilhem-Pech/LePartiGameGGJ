03a862d43dde304861903934ca44d216881d7b6d
Guilhem PECH <guilhem.pech@gmail.com>
2020-02-08 16:15:03 +0100

    Stop Ambiance when credit

--- Assets/States/Environement/EndTitle/OnCredit.cs
+++ Assets/States/Environement/EndTitle/OnCredit.cs
@@ -4,6 +4,11 @@ namespace States.Environement.EndTitle
 {
     public class OnCredit : StateMachineBehaviour
     {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            AkSoundEngine.PostEvent("Stop_Ambiance_Calme", null);
+        }
+
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             Application.Quit();
