472f25339556951e778afbce53b4783e042965db
Guilhem PECH <guilhem.pech@gmail.com>
2020-02-10 16:31:23 +0100

    Success UI Sounds

--- Assets/States/OnSuccessUI.cs
+++ Assets/States/OnSuccessUI.cs
@@ -5,11 +5,12 @@ namespace States
     public class OnSuccessUI : StateMachineBehaviour
     {
         public AK.Wwise.Event myEvent;
+        public AK.Wwise.Event uiEvent;
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
             int layerIndex)
         {
-            AkSoundEngine.PostEvent(myEvent.Id,animator.gameObject);
-            
+            myEvent.Post(animator.gameObject);
+            uiEvent.Post(animator.gameObject);
         }
     }
 }
